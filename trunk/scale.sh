dadf3a041ddff42807041212fda85d174c757503
arpruss@gmail.com <arpruss@gmail.com@e47ec320-7550-8fd2-8071-7005ffb565bd>
2012-08-08 02:47:44 +0000

    stylistic changes

---
+++ trunk/scale.sh
@@ -0,0 +1,4 @@
+cd res/drawable-xhdpi
+convert icon.png -resize 72x72 ../drawable-hdpi/icon.png
+convert icon.png -resize 48x48 ../drawable-mdpi/icon.png
+convert icon.png -resize 36x36 ../drawable-ldpi/icon.png
